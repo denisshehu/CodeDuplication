911b65c331bfa9388d90e3dfa4552a762c5c048f
AlexOdyn <gamerovich96@gmail.com>
2021-03-06 20:52:27 +0100

    Added code to compare all versions of jQuery with each other using jsInspect

--- src/main.py
+++ src/main.py
@@ -23,3 +23,16 @@ for i in range(1, len(jQuery_versions)):
 
 # for cell in heatmap_cells:
 #     print(cell.rowVersion.version_number + ': ' + cell.columnVersion.version_number)
+
+# For each version of jQuery, compare it with other versions of jQuery for clones
+for version1 in jQuery_versions:
+    for version2 in jQuery_versions:
+        # We do not compare same version to itself
+        if version1 != version2:
+            # -I is used to not match identifiers
+            # --ignore "intro|outro" does not compare intro.js and outro.js files as they produce errors
+            os.system('jsinspect -I -r json --ignore "intro|outro"'
+                      ' ../jquery-data/' + version1.version_number + '/src' +
+                      '../jquery-data/' + version2.version_number + '/src' +
+                      ' > clones_' + version1.version_number + '_vs_' + version2.version_number + '.json')
+            # Note that the output file is: 'clones_'+version1.version_number+'_vs_'+version2.version_number+'.json')
